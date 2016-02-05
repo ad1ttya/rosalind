9e29dfc22ee2ba6693cea21bf96fa7e0b1d24246
Adittya G <ad1ttya@hotmail.com>
2016-02-05 23:54:26 +0530

    Completed GC Content

---
+++ GC.fsx
@@ -0,0 +1,24 @@
+// Computing GC Content
+
+open System
+open System.IO
+
+let sample = File.ReadAllText "data/rosalind_gc.txt"
+
+let inputs = sample.Split '>' |> Array.tail |> Array.map (fun i ->
+    let data = i.Trim()
+    let id = data.Remove(13)
+    let sequence = data.Substring(14).Replace(System.Environment.NewLine, "").Normalize() //Newlines are the devil!!! :O
+    (id, sequence))
+
+let gcContent = inputs |> Array.map (fun (id, sequence) ->
+    let allCount = sequence.Length - 1
+        
+    let gcCount = sequence.Replace("A", "").Replace("T", "").Length - 1
+    
+    let percentage =  ((gcCount |> double) / (allCount |> double)) * 100.
+    (id, percentage))
+
+let largestID, largestPercent = gcContent |> Array.sortBy (fun (id, percent) -> percent) |> Array.last
+
+sprintf "%s\n%f" largestID largestPercent
